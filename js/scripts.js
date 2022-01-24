b0a41f49962a1a1c47e530f4eb88c723b4076aa1
Ryan Bass <theryanbass@gmail.com>
2022-01-24 11:53:18 -0800

    first commit
    Co-Authored-by: Ryan Bass <theryanbass@gmail.com>
    Co-authored-by: Matt Eilar <meilar@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business Logic
+function ToDo(item, dueDate, assignTo, check){
+  this.item = item;
+  this.dueDate = dueDate;
+  this.assignTo = assignTo;
+  this.check = check;
+}
+
+
+
+
+// User Interface
