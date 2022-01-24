9bdc477c9362e0be8e182f0c3c25be44ba19aafd
Ryan Bass <theryanbass@gmail.com>
2022-01-24 15:50:41 -0800

    add for loop to delete checked boxes
    Co-Authored-by: Ryan Bass <theryanbass@gmail.com>
    Co-authored-by: Matt Eilar <meilar@gmail.com>;

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,22 @@ ToDoList.prototype.assignId = function() {
 
 let userList = new ToDoList();
 
+// delete button creates new blank array, then function to check for checkboxes, some way to get a value relating to the todolist index, then we have an array full of index numbers, for each loop to delete from todolist and remove/hide css class based also on index number
+
 // User Interface
+
+function getText(index) {
+  let entryItem = userList.toDos[index];
+  let outputText = " " + entryItem.toDo + " [Due on: " + entryItem.dueDate + "] Assigned to: " + entryItem.assignTo;
+  return outputText;
+}
+
+function displayTasks() {
+  let index = userList.currentId;
+  let taskClass = "to-do-" + index;
+  let taskText = getText(index);
+  $("#main-task-list").append("<li class='" + taskClass + "'><input class='" + taskClass + "' type='checkbox'>" + taskText + "</input></li>");
+}
 
 function submitTask() {
   let taskIn = $("#task-name").val();
@@ -35,10 +50,41 @@ function submitTask() {
     return userList;
 }
 
+// $("input[type='checkbox']").val();
+
+// if ($('#check_id').is(":checked"))
+//{
+  // it is checked
+//}
+
+// const toDoCheck = {
+//   blah: blahblah;
+//   blah2: blahblah
+// }
+// Object.keys(toDoCheck).forEach(key => {
+
+// })
+
 
 $(document).ready(function(event) {
   $("#task-entry").submit(function(event){
     event.preventDefault();
     submitTask();
+    displayTasks();
+    $(".status").show();
+  })
+  $("#reset-btn").click(function(event) {
+    event.preventDefault();
+    console.log("reset button is triggering");
+     for (let i = 0; i <= userList.currentId; i++) {
+       console.log(userList.currentId);
+      if ($('input.to-do-' + i).is(":checked")) {
+        console.log("for loop is triggering")
+        $('.to-do-' + i).remove();
+      }
+    }
+    
   })
 })
+
+
